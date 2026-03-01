0211efd0622be2125bbd724650bbaf10a3f5ad4f
Doğa <dogabeey@gmail.com>
2026-03-01 22:33:08 +0300

    match checking algorithm

--- Assets/Scripts/Framework/Grid/Match3Grid.cs
+++ Assets/Scripts/Framework/Grid/Match3Grid.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
 
 namespace Game
 {
@@ -15,6 +16,11 @@ namespace Game
         public override void PostInit()
         {
             // Custom post-initialization logic for Match-3 grid
+        }
+
+        public void MatchProcess()
+        {
+
         }
 
         public void SwapElements(Vector2Int first, Vector2Int second)
@@ -65,6 +71,148 @@ namespace Game
                 secondElement.transform.SetParent(firstTile, false);
                 secondElement.transform.DOLocalMove(Vector3.zero, 0.3f).SetEase(Ease.OutBack);
             }
+        }
+
+        public List<List<Vector2Int>> CheckMatchOf(int elementCount = 3)
+        {
+            Dictionary<Vector2Int, ElementData> matchedElements = new Dictionary<Vector2Int, ElementData>();
+
+            ElementData GetElementData(int x, int z)
+            {
+                return gridElements.TryGetValue(new Vector3Int(x, 0, z), out GridElementInfo info)
+                    ? info.elementData
+                    : null;
+            }
+
+            void AddMatched(int x, int z, ElementData data)
+            {
+                Vector2Int pos = new Vector2Int(x, z);
+                if (!matchedElements.ContainsKey(pos))
+                {
+                    matchedElements.Add(pos, data);
+                }
+            }
+
+            for (int z = 0; z < gridSize.z; z++)
+            {
+                ElementData currentData = null;
+                int runLength = 0;
+
+                for (int x = 0; x < gridSize.x; x++)
+                {
+                    ElementData data = GetElementData(x, z);
+
+                    if (data != null && data == currentData)
+                    {
+                        runLength++;
+                        continue;
+                    }
+
+                    if (currentData != null && runLength >= elementCount)
+                    {
+                        for (int matchX = x - runLength; matchX < x; matchX++)
+                        {
+                            AddMatched(matchX, z, currentData);
+                        }
+                    }
+
+                    currentData = data;
+                    runLength = data != null ? 1 : 0;
+                }
+
+                if (currentData != null && runLength >= elementCount)
+                {
+                    for (int matchX = gridSize.x - runLength; matchX < gridSize.x; matchX++)
+                    {
+                        AddMatched(matchX, z, currentData);
+                    }
+                }
+            }
+
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                ElementData currentData = null;
+                int runLength = 0;
+
+                for (int z = 0; z < gridSize.z; z++)
+                {
+                    ElementData data = GetElementData(x, z);
+
+                    if (data != null && data == currentData)
+                    {
+                        runLength++;
+                        continue;
+                    }
+
+                    if (currentData != null && runLength >= elementCount)
+                    {
+                        for (int matchZ = z - runLength; matchZ < z; matchZ++)
+                        {
+                            AddMatched(x, matchZ, currentData);
+                        }
+                    }
+
+                    currentData = data;
+                    runLength = data != null ? 1 : 0;
+                }
+
+                if (currentData != null && runLength >= elementCount)
+                {
+                    for (int matchZ = gridSize.z - runLength; matchZ < gridSize.z; matchZ++)
+                    {
+                        AddMatched(x, matchZ, currentData);
+                    }
+                }
+            }
+
+            List<List<Vector2Int>> groups = new List<List<Vector2Int>>();
+            HashSet<Vector2Int> visited = new HashSet<Vector2Int>();
+
+            foreach (var kvp in matchedElements)
+            {
+                if (visited.Contains(kvp.Key))
+                {
+                    continue;
+                }
+
+                List<Vector2Int> group = new List<Vector2Int>();
+                Queue<Vector2Int> queue = new Queue<Vector2Int>();
+                queue.Enqueue(kvp.Key);
+                visited.Add(kvp.Key);
+
+                while (queue.Count > 0)
+                {
+                    Vector2Int pos = queue.Dequeue();
+                    group.Add(pos);
+                    ElementData data = matchedElements[pos];
+
+                    Vector2Int[] neighbors =
+                    {
+                        new Vector2Int(pos.x + 1, pos.y),
+                        new Vector2Int(pos.x - 1, pos.y),
+                        new Vector2Int(pos.x, pos.y + 1),
+                        new Vector2Int(pos.x, pos.y - 1)
+                    };
+
+                    foreach (Vector2Int neighbor in neighbors)
+                    {
+                        if (matchedElements.TryGetValue(neighbor, out ElementData neighborData) &&
+                            neighborData == data &&
+                            !visited.Contains(neighbor))
+                        {
+                            visited.Add(neighbor);
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+
+                if (group.Count >= elementCount)
+                {
+                    groups.Add(group);
+                }
+            }
+
+            return groups;
         }
     }
 }
