4e4660f94b2248eb95a3249fcb886e3f1e514cc6
dogabeey <dogabeey@gmail.com>
2026-03-27 01:28:57 +0300

    wall break anim

--- Assets/Scripts/Framework/Grid/BreakableWall.cs
+++ Assets/Scripts/Framework/Grid/BreakableWall.cs
@@ -5,9 +5,29 @@ namespace Game
 {
     public class BreakableWall : GridCellController
     {
+        [SerializeField] private Animator wallAnimator;
+        [SerializeField] private string breakTriggerName = "Break";
+        [SerializeField] private float breakAnimationDuration = 0.25f;
+
+        private void Awake()
+        {
+            if (wallAnimator == null)
+            {
+                wallAnimator = GetComponent<Animator>();
+            }
+        }
+
         public IEnumerator WallBreak()
         {
-            yield break;
+            if (wallAnimator != null && !string.IsNullOrEmpty(breakTriggerName))
+            {
+                wallAnimator.SetTrigger(breakTriggerName);
+
+                if (breakAnimationDuration > 0f)
+                {
+                    yield return new WaitForSeconds(breakAnimationDuration);
+                }
+            }
         }
     }
 }
