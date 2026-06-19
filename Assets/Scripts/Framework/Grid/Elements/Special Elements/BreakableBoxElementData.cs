8e54419cb4335ba830cd39af5df14053eacb5873
Doğa <dogabeey@gmail.com>
2026-06-19 16:16:08 +0300

    breakable box element data (WIP)

---
+++ Assets/Scripts/Framework/Grid/Elements/Special Elements/BreakableBoxElementData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+/// <summary>
+/// Breakable Boxes work like Breakable Walls, but they are in element layer.
+/// </summary>
+[CreateAssetMenu(fileName = "BreakableBoxElementData", menuName = "Game/Elements/Special/Breakable Box Element Data...")]
+public class BreakableBoxElementData : ElementData
+{
+}
--- Assets/Scripts/Framework/Grid/Elements/Special Elements/GarbageBagElementData.cs
+++ Assets/Scripts/Framework/Grid/Elements/Special Elements/GarbageBagElementData.cs
@@ -4,11 +4,3 @@ using UnityEngine;
 public class GarbageBagElementData : ElementData
 {
 }
-
-/// <summary>
-/// Breakable Boxes work like Breakable Walls, but they are in element layer.
-/// </summary>
-[CreateAssetMenu(fileName = "BreakableBoxElementData", menuName = "Game/Elements/Special/Breakable Box Element Data...")]
-public class BreakableBoxElementData : ElementData
-{
-}
