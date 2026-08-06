c7b2c4f998a3caef2117d5bf5cca9efd3ad9fd46
Doğa <dogabeey@gmail.com>
2026-08-06 16:29:44 +0300

    lose panel ui changes

--- Assets/Scripts/Framework/ScreenManagement/LoseScreen.cs
+++ Assets/Scripts/Framework/ScreenManagement/LoseScreen.cs
@@ -12,8 +12,6 @@ namespace Game
         public override Screens ScreenID => Screens.LoseScreen;
 
         public TMP_Text levelHeaderText;
-        public TMP_Text levelLostReason;
-        public TMP_Text addMovesText;
         public TMP_Text addMovesCost;
         public Button addMovesButton;
         public Button repeatLevelButton;
@@ -33,8 +31,6 @@ namespace Game
             base.InitUI(eventParam);
             LevelScene_Match3Game levelScene = GameManager.Instance.CurrentLevel as LevelScene_Match3Game;
             if(levelHeaderText) levelHeaderText.text = string.Format(levelHeaderFormat, GameManager.Instance.CurrentLevelIndex + 1);
-            if(levelLostReason) levelLostReason.text = "OUT OF MOVES!";
-            if(addMovesText) addMovesText.text = string.Format(addMovesButtonFormat, levelScene.extraMovesGiven);
             if(addMovesCost) addMovesCost.text = string.Format(addMovesCostFormat, levelScene.extraMoveCost.amount, levelScene.extraMoveCost.type.spriteIndexForUI);
 
             addMovesButton.interactable = CanAddMovesOrTime();
